2fb94199b64f12a5320b32ebb2fc3a8a39f3d734
Wendell <wendell@Wendells-Mac-mini.local>
2019-10-14 13:17:47 +0800

    add comments db schema

--- dbschemas.js
+++ dbschemas.js
@@ -14,11 +14,21 @@ let db = {
 
   screams: [
     {
+      screamId: '6TklGIgw6CopOKCE9Zs9',
       userHandle: 'user',
       body: 'this is a body',
       createdAt: '2019-10-10T04:58:34.642Z',
       likeCount: 5,
       commentCount: 2,
+    },
+  ],
+
+  comments: [
+    {
+      userHandle: 'user',
+      screamId: '6TklGIgw6CopOKCE9Zs9',
+      body: 'nice one, mate',
+      createdAt: '2019-10-11T06:29:16.317Z',
     },
   ],
 };
